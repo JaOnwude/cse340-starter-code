6587d424c4c65c964b438ec8fa95db53b87a0752
JaOnwude <ojamesuchenna61@gmail.com>
2025-09-21 21:05:06 +0100

    A total overhaul carried out

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -8,7 +8,6 @@ async function getClassifications(){
 }
 
 
-
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
  * ************************** */
@@ -27,20 +26,4 @@ async function getInventoryByClassificationId(classification_id) {
   }
 }
 
-/* ***************************
- *  Get inventory and classification data by inv_id
- *   * ************************** */
-async function getInventoryById(invId) {
-  try {
-    const data = await pool.query(
-      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.inv_id = $1",
-      [invId]
-    )
-    return data.rows[0]
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-
-module.exports = {getClassifications, getInventoryByClassificationId,getInventoryById};
+module.exports = {getClassifications, getInventoryByClassificationId}
